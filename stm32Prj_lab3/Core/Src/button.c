d43c27f0cfb7d10729bf201cdf3f3d5530985b4c
Vinh Phat <caovinhphat1904@gmail.com>
2024-10-26 23:37:36 +0700

    still updating

--- stm32Prj_lab3/Core/Src/button.c
+++ stm32Prj_lab3/Core/Src/button.c
@@ -7,11 +7,11 @@
 
 #include "button.h"
 
-#define NoPress 0
-#define PressRelease 1
-#define PressOver 2
+#define NO_PRESS 0
+#define PRESS_RELEASE 1
+#define PRESS_OVER 2
 
-//3 key reg for avoiding vibrate
+//3 keyreg for avoiding vibrate
 int KeyReg0 = NORMAL_STATE;
 int KeyReg1 = NORMAL_STATE;
 int KeyReg2 = NORMAL_STATE;
@@ -19,38 +19,66 @@ int KeyReg2 = NORMAL_STATE;
 //Normal state before
 int KeyReg3 = NORMAL_STATE;
 
-int TimeOutForKeyPress =  200;	//Time for PressOver
+int TimeOutForKeyPress =  200;	//Time for PRESS_OVER
 int button1_flag = 0;			//Flag for button signal
 
+//void getKeyInput(){
+//	KeyReg0 = KeyReg1;
+//	KeyReg1 = KeyReg2;
+//	KeyReg2 = HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin);
+//	if((KeyReg0 == KeyReg1) && (KeyReg1 == KeyReg2)){
+//		if(KeyReg3 != KeyReg2){	//Case PRESS_RELEASE
+//			KeyReg3 = KeyReg2;
+//			if(KeyReg3 == PRESSED_STATE){
+//				TimeOutForKeyPress = 200;
+//				button1_flag = 1;
+//			}
+//		}else{	//Case PRESS_OVER
+//			TimeOutForKeyPress--;
+//			if(TimeOutForKeyPress == 0){
+//				TimeOutForKeyPress = 200;
+//				if(KeyReg3 == PRESSED_STATE){
+//					button1_flag = 1;
+//				}
+//			}
+//		}
+//	}
+//}
+
+int pressType = NO_PRESS;
 void getKeyInput(){
-	KeyReg2 = KeyReg1;
-	KeyReg1 = KeyReg0;
-	KeyReg0 = HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin);
+	KeyReg0 = KeyReg1;
+	KeyReg1 = KeyReg2;
+	KeyReg2 = HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin);
 	if((KeyReg0 == KeyReg1) && (KeyReg1 == KeyReg2)){
-		if(KeyReg3 != KeyReg2){	//Case PressRelease
-			KeyReg3 = KeyReg2;
-			if(KeyReg3 == PRESSED_STATE){
+		switch(pressType){
+		case NO_PRESS:
+			if(KeyReg3 != KeyReg2){
+				KeyReg3 = KeyReg2;
+				pressType = PRESS_RELEASE;
+			}else{
+				pressType = PRESS_OVER;
+			}
+
+			if(KeyReg3 == NORMAL_STATE){
+				pressType = NO_PRESS;
+			}
+			break;
+		case PRESS_RELEASE:
 				TimeOutForKeyPress = 200;
 				button1_flag = 1;
-			}
-		}else{	//Case PressOver
+				pressType = NO_PRESS;
+			break;
+		case PRESS_OVER:
 			TimeOutForKeyPress--;
 			if(TimeOutForKeyPress == 0){
 				TimeOutForKeyPress = 200;
-				if(KeyReg3 == PRESSED_STATE){
-					button1_flag = 1;
-				}
+				button1_flag = 1;
+				pressType = NO_PRESS;
 			}
+			break;
+		default:
+			break;
 		}
-	}
-}
-
-int typePress = NoPress;
-void getKeyInput(){
-	KeyReg2 = KeyReg1;
-	KeyReg1 = KeyReg0;
-	KeyReg0 = HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin);
-	if((KeyReg0 == KeyReg1) && (KeyReg1 == KeyReg2)){
-
 	}
 }
