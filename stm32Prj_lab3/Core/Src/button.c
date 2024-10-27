d9b83fb8f5c1f12ade870d9319fd419d5a0f03ca
Vinh Phat <caovinhphat1904@gmail.com>
2024-10-28 06:57:31 +0700

    using fsm in button

--- stm32Prj_lab3/Core/Src/button.c
+++ stm32Prj_lab3/Core/Src/button.c
@@ -7,41 +7,50 @@
 
 #include "button.h"
 
-#define NO_PRESS 0
-#define PRESS_RELEASE 1
-#define PRESS_OVER 2
-
-//3 keyreg for avoiding vibrate
-int KeyReg0 = NORMAL_STATE;
-int KeyReg1 = NORMAL_STATE;
-int KeyReg2 = NORMAL_STATE;
-
-//Normal state before
-int KeyReg3 = NORMAL_STATE;
+#define ZERO 				0
+#define PRESS_RELEASE_1 	1
+#define PRESS_RELEASE_2 	2
+#define PRESS_RELEASE_3 	3
+#define PRESS_OVER   		4
 
 int TimeOutForKeyPress =  200;	//Time for PRESS_OVER
 int button1_flag = 0;			//Flag for button signal
 
+int pressType = ZERO;
 void getKeyInput(){
-	KeyReg0 = KeyReg1;
-	KeyReg1 = KeyReg2;
-	KeyReg2 = HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin);
-	if((KeyReg0 == KeyReg1) && (KeyReg1 == KeyReg2)){
-		if(KeyReg3 != KeyReg2){	//Case PRESS_RELEASE
-			KeyReg3 = KeyReg2;
-			if(KeyReg3 == PRESSED_STATE){
-				TimeOutForKeyPress = 200;
-				button1_flag = 1;
-			}
-		}else{	//Case PRESS_OVER
-			TimeOutForKeyPress--;
-			if(TimeOutForKeyPress == 0){
-				TimeOutForKeyPress = 200;
-				if(KeyReg3 == PRESSED_STATE){
-					button1_flag = 1;
-				}
-			}
+	switch(pressType){
+	case ZERO:
+		if(HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin) == NORMAL_STATE){
+			return;
 		}
+		pressType = PRESS_RELEASE_1;
+		break;
+	case PRESS_RELEASE_1:
+		if(HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin) == NORMAL_STATE){
+			pressType = ZERO;
+			return;
+		}
+		pressType = PRESS_RELEASE_2;
+		break;
+	case PRESS_RELEASE_2:
+		if(HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin) == NORMAL_STATE){
+			pressType = ZERO;
+			return;
+		}
+		button1_flag = 1;
+		pressType = PRESS_OVER;
+		TimeOutForKeyPress = 200;
+		break;
+	case PRESS_OVER:
+		--TimeOutForKeyPress;
+		if(TimeOutForKeyPress == 0){
+			TimeOutForKeyPress = 200;
+			button1_flag = 1;
+		}
+		if(HAL_GPIO_ReadPin(button1_GPIO_Port, button1_Pin) == NORMAL_STATE){
+			pressType = ZERO;
+		}
+	default:
+		break;
 	}
 }
-
